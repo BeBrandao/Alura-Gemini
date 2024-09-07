d90058c5bf679e34a2cc565e0a549078d75dc732
Bebrandao <beabs2305@gmail.com>
2024-09-07 00:46:22 -0300

    finalizando

--- app.js
+++ app.js
@@ -1,51 +1,70 @@
 function pesquisar() {
     let section = document.getElementById("resultados-pesquisa");
-console.log(section);
 
-let resultados = ""
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-for (let dado of dados) {
-    resultados += `
-        <div class="item-resultado" id="${dado.id}">
-        <img src="${dado.imagem}" alt="${dado.titulo}">
-        <div class="item-alinhamento">
-            <h2>${dado.titulo}</h2>
-            <table class="descricao-meta">
-                <tr>
-                    <td>${dado.musicas[0]}</td>
-                    <td>${dado.musicas[1]}</td>
-                    <td>${dado.musicas[2]}</td>
-                </tr>
-                <tr>
-                    <td>${dado.musicas[3]}</td>
-                    <td>${dado.musicas[4]}</td>
-                    <td>${dado.musicas[5]}</td>
-                </tr>
-                <tr>
-                    <td>${dado.musicas[6]}</td>
-                    <td>${dado.musicas[7]}</td>
-                    <td>${dado.musicas[8]}</td>
-                </tr>
-                <tr>
-                    <td>${dado.musicas[9]}</td>
-                    <td>${dado.musicas[10]}</td>
-                    <td>${dado.musicas[11]}</td>
-                </tr>
-                <tr>
-                    <td>${dado.musicas[12]}</td>
-                    <td>${dado.musicas[13]}</td>
-                    <td>${dado.musicas[14]}</td>
-                </tr>
-                <tr>
-                    <td>${dado.musicas[15]}</td>
-                    <td>${dado.musicas[16]}</td>
-                    <td>${dado.musicas[17]}</td>
-                </tr>
-            </table>
-        </div>
-        </div>
-`
-}
-section.innerHTML = resultados
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado<p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let tags = "";
+
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if (titulo.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+                <div class="item-resultado" id="${dado.id}">
+                <img src="${dado.imagem}" alt="${dado.titulo}">
+                <div class="item-alinhamento">
+                    <h2>${dado.titulo}</h2>
+                    <table class="descricao-meta">
+                        <tr>
+                            <td>${dado.musicas[0]}</td>
+                            <td>${dado.musicas[1]}</td>
+                            <td>${dado.musicas[2]}</td>
+                        </tr>
+                        <tr>
+                            <td>${dado.musicas[3]}</td>
+                            <td>${dado.musicas[4]}</td>
+                            <td>${dado.musicas[5]}</td>
+                        </tr>
+                        <tr>
+                            <td>${dado.musicas[6]}</td>
+                            <td>${dado.musicas[7]}</td>
+                            <td>${dado.musicas[8]}</td>
+                        </tr>
+                        <tr>
+                            <td>${dado.musicas[9]}</td>
+                            <td>${dado.musicas[10]}</td>
+                            <td>${dado.musicas[11]}</td>
+                        </tr>
+                        <tr>
+                            <td>${dado.musicas[12]}</td>
+                            <td>${dado.musicas[13]}</td>
+                            <td>${dado.musicas[14]}</td>
+                        </tr>
+                        <tr>
+                            <td>${dado.musicas[15]}</td>
+                            <td>${dado.musicas[16]}</td>
+                            <td>${dado.musicas[17]}</td>
+                        </tr>
+                    </table>
+                </div>
+                </div>
+        `
+        }
+    }
+    
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+    
+    section.innerHTML = resultados
 
 }
--- dados.js
+++ dados.js
@@ -3,18 +3,21 @@ let dados = [
         titulo: "Utopia",
         musicas: ["HYENA", "MELTDOWN", "PARASAIL", "THANKGOD", "FE!N", "SKITZO", "MODERN JAM", "DELRESTO(ECHOES)", "LOST FOREVER", "MY EYES", "I KNOW?", "LOOOVE", "GOD'S COUNTRY", "TOPIA TWINS", "K-POP", "SIRENS", "CIRCUS MAXIMUS", "TIL FURTHER NOTICE"],
         imagem: "Utopia.jpg",
-        id: "Utopia"
+        id: "Utopia",
+        tags: "hyenameltdownparasailthankgodfe!nskitzomodernjamdelresto(echoes)lostforevermyeyesiknow?looovegodscoountrytopiawinskpopsirenccircusmaximustilfurthernotice",
     },
     {
         titulo: "Astroworld",
         musicas: ["STARGAZING", "SKELETONS", "WHO?WHAT", "CAROUSEL", "WAKE UP", "BUTTERFLY EFFECT", "SICKO MODE", "5% TINT", "HOUSTONFORNICATION", "R.I.P. SCREW", "NC-17", "COFFEE BEAN", "STOP TRYING TO BE GOD", "ASTROTHUNDER", "NO BYSTANDERS", "CAN'T SAY", "", ""],
         imagem: "astroworld.jpg",
-        id: "Astroworld"
+        id: "Astroworld",
+        tags: "stargazingskeletonswho?whatcarouselwakeupbutterflyeffectsickomode5%tinthoustonfornicationr.i.p.screwnc-17coffeebeanstoptryingtobegodastrothundernobystandersscan'tsay",
     },
     {
-        titulo: "DBR",
+        titulo: "Days Before Rodeo",
         musicas: ["Days Before Rodeo: The Prayer", "Zombies", "Mamacita", "Sloppy Toppy", "Quintana Pt.2", "Basement Freestyle", "Drugs You Should Try It", "Backyard", "Don't Play", "Grey", "Skyfall", "BACC", "", "", "", "", "", ""],
         imagem: "Days-Before-Rodeo.jpg",
-        id: "DBR"
+        id: "DBR",
+        tags: "daysbeforerodeotheprayerzombiesmamacitasloppytoppyquintana pt.2basementfreestyledrugsyoushouldtryitbackyarddontplayskyfallbacc",
     }
 ];
